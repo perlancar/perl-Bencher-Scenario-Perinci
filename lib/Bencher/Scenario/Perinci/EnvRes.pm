8cf955e5e11b7a237e4223ce08e4a9b7157956bf
perlancar (@pc-home) <perlancar@gmail.com>
2015-12-31 15:33:38 +0700

    Add scenario: Perinci::EnvRes (ported from bench-envres from App-BenchPerinci)

---
+++ lib/Bencher/Scenario/Perinci/EnvRes.pm
@@ -0,0 +1,18 @@
+package Bencher::Scenario::Perinci::EnvRes;
+
+# DATE
+# VERSION
+
+our $scenario = {
+    summary => 'Compare returning enveloped result vs naked/list',
+    participants => [
+        {name=>'envres'             , code_template => 'return [200, "OK", "foo"]'},
+        {name=>'envres_with_resmeta', code_template => 'return [200, "OK", "foo", {}]'},
+        {name=>'str'                , code_template => 'return "foo"'},
+        {name=>'list'               , code_template => 'return (200, "OK", "foo")', result_is_list=>1},
+    ],
+};
+
+# ABSTRACT:
+
+=head1 SYNOPSIS
